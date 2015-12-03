72a0195a92d78b79787a28f49a94994b86470a83
Izuroxx <izu.i.cnr@gmail.com>
2015-12-03 11:11:39 +0100

    Changes_var

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-    
+    @post[:user_id] = @current_user
 
     respond_to do |format|
       if @post.save
@@ -71,6 +71,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :body, :user_id)
+      params.require(:post).permit(:title, :body)
     end
 end
