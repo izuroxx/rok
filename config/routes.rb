5191607c0f6122ad18dbc268daaab2d9dcfa1c48
agnik <reira@wp.pl>
2015-12-06 20:04:50 +0000

    Log in on the main site, welcome index added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
+  get 'welcome/index'
+
 get    'login'   => 'sessions#new'
 post   'login'   => 'sessions#create'
 delete 'logout'  => 'sessions#destroy'
 
-root "sessions#new"
+root 'welcome#index'
 
   resources :posts
 
